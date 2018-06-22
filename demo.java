9255b8e28bd35722188c785862e9bbe59b685cf6
toandn96 <40200541+toandn96@users.noreply.github.com>
2018-06-22 09:29:55 +0700

    Update demo.java

--- demo.java
+++ demo.java
@@ -23,3 +23,19 @@ import jp.co.melb.pp.es.common.MessageUtil;
 import jp.co.melb.pp.es.common.ParameterUtil;
 import jp.co.melb.pp.es.common.log.AccessLogWriter;
 import jp.co.melb.pp.es.common.log.ApplicationProcessingLogWriter;
+/**
+ * 利用者認証API: 利用者認証サービスでは、電子申請ツール等のクライアントからの要求を受けて、利用者認証を行う.
+ * <p>
+ * また、認証結果およびアクセスキーをクライアントへ通知する。
+ * </p>
+ * <p>
+ * e-Govシステムでは、登録済みの利用者IDであること、証明書の識別情報と利用者IDの対応関係の確認が行われる。
+ * </p>
+ * <p>
+ * 利用する外部連携APIは「利用者認証」である。
+ * </p>
+ * 
+ * @author van-nttc
+ * @version 1.0.0
+ *
+ */
